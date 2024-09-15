18e6fc523ccf243d5bbaa99cff7958c67fc3b995
sgmkhr <mksghr10@gmail.com>
2024-09-15 18:37:15 +0900

    Initial Commit

---
+++ practice_camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_camera
+//
+//  Created by 菅原実希 on 2024/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
